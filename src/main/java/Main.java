400e7fe5ed9baaafc9e541321fc9133cd4799086
Michal <mnowinski@outlook.com>
2016-11-16 14:24:56 +0100

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by Michal N. on 2016-11-16.
+ */
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start(Stage primaryStage) throws Exception {
+
+
+    }
+}
